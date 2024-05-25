cba62c89c6689345f9924e69a240f9df23413d5b
MogaDenis <moga_denis@yahoo.com>
2024-05-25 21:23:55 +0300

    Small change on the BE

--- MPP-Backend.Data/Repositories/UserRepository.cs
+++ MPP-Backend.Data/Repositories/UserRepository.cs
@@ -69,6 +69,7 @@ namespace MPP_Backend.Data.Repositories
             newUser.Password = hashedPassword;
 
             _context.Users.Entry(userToUpdate).CurrentValues.SetValues(newUser);
+            await _context.SaveChangesAsync();
 
             return true;
         }
